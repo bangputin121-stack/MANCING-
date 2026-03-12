80ead903fe21a8f1ee9875aed57eab800863a3cf
bangputin121-stack <bangputin121@gmail.com>
2026-03-12 16:32:51 +0700

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -1,1 +1,15 @@
+# Daftar Ikan, Harga Jual, dan Kelangkaan
+FISH_DATA = {
+    "Teri": {"price": 10, "chance": 50},
+    "Lele": {"price": 50, "chance": 30},
+    "Nila": {"price": 100, "chance": 15},
+    "Paus": {"price": 1000, "chance": 4},
+    "Nemo": {"price": 5000, "chance": 1}
+}
 
+# Daftar Joran (Nanti buat di fitur Shop)
+FISHING_RODS = {
+    "Bambu": {"bonus": 0, "price": 0},
+    "Serat Karbon": {"bonus": 5, "price": 2000},
+    "Berlian": {"bonus": 15, "price": 10000}
+}
